51dc698673f96911fdbcd5b51ddf174945c9d548
AkashLal-28 <113417847+AkashLal-28@users.noreply.github.com>
2024-05-08 15:47:56 +0530

    first commit

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    context = {'home':'active'}
+    return render(request, "core/home.html", context)
+def contact(request):
+    return render(request, "core/contact.html", {'contact':'active'})
